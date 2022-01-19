5d24b6153c8100d0cc1697a9d15d85baf5ebc656
arvinlauron <arvinglauron@gmail.com>
2022-01-19 13:21:57 +0800

    first development commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import Post from './Post';
+import NewPost from './NewPost';
+import './App.css';
+
+
+function App() {
+  const [posts] = useState(
+    [
+      {key: 1, name: "Gian", content:"Gwapa kay ka", likes:2},
+      {key: 2, name: "Shad", content:"Pogi ko", likes:0},
+      {key: 3, name: "Carlos", content:"Hanging around", likes:0},
+      {key: 4, name: "Reyner", content:"Pagod na ako!", likes:0},
+    ]
+  );
+
+    return (
+      <div>
+        <div className='app'>
+          {posts.map(post => (
+            <Post key={post.key} name={post.name} content={post.content} likes={post.likes}  />
+          ))}
+           
+        </div>
+        <div className='app'>
+          <NewPost />
+        </div>       
+      </div>
+    );
+    
+  }
+
+ export default App;
---
+++ src/LikeButton.js
@@ -0,0 +1,18 @@
+import React, {useState} from 'react';
+
+ function LikeButton (props){
+    const{likes} = props
+    const[count, setCount] = useState(likes)
+
+    const increment = () => {
+        setCount(count + 1);
+    }
+
+        return (
+            <div>
+                <p>{count}</p>
+                <button onClick={increment}>Like</button>
+            </div>
+        )
+    }
+export default LikeButton
