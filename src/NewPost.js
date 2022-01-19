5e52957d645f6c06cfadd4cec1a392fa1e204fd9
arvinlauron <arvinglauron@gmail.com>
2022-01-19 13:26:30 +0800

    update

--- src/NewPost.js
+++ src/NewPost.js
@@ -5,19 +5,18 @@ import './App.css';
 function App(){
     const[name,SetName] = useState("");
     const[content,SetContent] = useState("");
-    const[likes,SetLikes] = useState("");
     const[list,setList]=useState([])
     
 
     const handleSubmit=(e)=>{
         e.preventDefault();
         // console.log(name,content,likes)
-        const data={name,content,likes}
-        if(name&&content&&likes){
+        const data={name,content,likes:0}
+        if(name&&content){
             setList((ls)=>[...ls,data])
             SetName("")
             SetContent("")
-            SetLikes("")
+          
 
         }
     }
@@ -33,7 +32,6 @@ function App(){
             <form onSubmit={handleSubmit}>
                 <input type="text" name = "name" value={name} placeholder='Enter a name' onChange={e=>SetName(e.target.value)}  />
                 <input type="text" name = "content" value={content} placeholder='Enter a post' onChange={e=>SetContent(e.target.value)}  />
-                <input type="number" name = "likes" value={likes} placeholder='Enter a number' onChange={e=>SetLikes(e.target.value)} />
                 <button>Post</button>
             </form>
             </div>
